42a40787e246e14151668e0d6d0c2018c84d634a
Harry Heckmann <harry@MacBook-Pro.local>
2019-03-19 13:41:20 -0500

    adds muitheme to index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,54 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from "react-router-dom"
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#EF4E4E',
+            main: '#EF4E4E',
+            dark: '#EF4E4E',
+            contrastText: 'rgb(0,0,0)'
+        }
+    },
+    typography: {
+        button: {
+            fontFamily: 'Vollkorn',
+        },
+        h2: {
+            fontFamily: 'Lobster'
+        },
+        h3: {
+            fontFamily: 'Lobster'
+        },
+        h4: {
+            fontFamily: ['Vollkorn', 'serif'],
+            color: '#FFFFFF',
+            fontWeight: "900",
+            textTransform: "none"
+        },
+        h5: {
+            fontFamily: ['Vollkorn', 'serif'],
+            color: '#FFFFFF',
+            fontWeight: "900",
+            textTransform: "none"
+        },
+        h6: {
+            fontFamily: 'Lato'
+        }
+    }
+})
+
+ReactDOM.render(
+
+    <Router>
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
+    </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
