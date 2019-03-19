08fb463d5ddee14b50daae205c83132eda744c6b
Yusef Daramay <yousufdaramay@yahoo.com>
2019-03-19 16:45:57 -0500

    created card for profile

---
+++ db/create_date_activity.sql
@@ -0,0 +1,2 @@
+INSERT INTO activity(activity_description,activity_photo,activity_google_place_id)
+VALUES($1,$2,$3);
---
+++ db/create_date_memory.sql
@@ -0,0 +1,2 @@
+INSERT INTO memory_maker(memory_description,memory_photo,memory_google_place_id)
+VALUES($1,$2,$3);
