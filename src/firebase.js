8330a79f414f94f1afcdd3310c074365ca1e8279
Harry Heckmann <harry@MacBook-Pro-2.local>
2019-03-21 16:30:48 -0500

    updates header with firebase auth

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { firebase } from '@firebase/app'
+import 'firebase/firebase-storage'
+
+var config = {
+        apiKey: "AIzaSyCXnqkV46yt0XYCa3ZhKvKVhEozpf8EofA",
+        authDomain: "group-project-e903d.firebaseapp.com",
+        databaseURL: "https://group-project-e903d.firebaseio.com",
+        projectId: "group-project-e903d",
+        storageBucket: "group-project-e903d.appspot.com",
+        messagingSenderId: "689677486989"
+    };
+    firebase.initializeApp(config);
+    // export const googleProvider = new firebase.auth.GoogleAuthProvider()
+    // export const provider = new firebase.auth.GoogleAuthProvider();
+    // export const auth = firebase.auth();
+
+
+    export default firebase
