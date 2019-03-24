435a1689e8d369c0fe86b1c0a0c84c91de852756
Harry Heckmann <harry@MacBook-Pro-2.local>
2019-03-24 17:57:08 -0500

    updates the search component to handle sending a post query to the backend

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from "react-dom";
 import SearchBar from './SearchBar'
 import Autocomplete from './Autocomplete'
+import Results from './Results'
 import Button from '@material-ui/core/Button'
 
 
 import './search.css'
 
 
+function Search(){
+  const [test, setTest] = useState('Howdy');
 
-class Search extends React.Component {
-  state = {
-      open: false
-  };
+  const [inputValue, setInputValue] = useState('')
+  const [selectedItem, setSelectedItem] = useState('')
+  const [location, setLocation] = useState({lat: '', lng: ''})
+  const [within, setWithin] = useState(25)
 
-  
-  render() {
-    return (
-      <div id='search_page'>
+  function handleClick(){
+    console.log('click')
+  }
+
+// const TestContext = React.createContext(null);
+// console.log(selectedItem)  
+  return (
+       <div id='search_page'>
         <div id='search_page_dim'>
           <div id='search_div'>
-            <Autocomplete/>
-            <SearchBar/>
-            <Button variant='contained' color='primary' style={{borderRadius: '0', maxHeight: '56px'}}>Search</Button>
+            <Autocomplete
+              location = {location}
+              setLocation = {setLocation}
+            />
+            <SearchBar
+              inputValue = {inputValue}
+              setInputValue = {setInputValue}
+              selectedItem = {selectedItem}
+              setSelectedItem = {setSelectedItem}
+              within = {within}
+              setWithin = {setWithin}
+            />
+            <Button variant='contained' color='primary' style={{borderRadius: '0', maxHeight: '56px'}} onClick={handleClick}>Search</Button>
           </div>
+          {/* <TestContext.Provider value={'test'}> */}
+            <Results 
+              test={test}
+              setTest={setTest}
+            />
+          {/* </TestContext.Provider> */}
         </div>
       </div>
-      
-    );
-  }
+  );
 }
 
 
