db9c79dcd61f02a9033c34352be13cfef56fb5f3
Harry Heckmann <harry@MacBook-Pro-2.local>
2019-03-26 13:18:17 -0500

    further testing in testing

--- tests/harry/Harry.test.js
+++ tests/harry/Harry.test.js
@@ -1,14 +1,28 @@
-// const {checkUser} = require('../../src/components/header/Header')
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+configure({ adapter: new Adapter() });
 
-// describe('Auth Tests', () => {
-//     afterEach(function() {
-//         jest.resetModules()
-//     })
-    
-//     test('succesfull auth should return true', () => {
-//         Header.checkUser()
+import React from 'react';
+// import { shallow, mount, render } from 'enzyme';
 
-//         expect(Header.signedIn).toEqual(true)
-//     })
-// })
+import Header from '../../src/components/header/Header';
+
+describe('A suite', function() {
+  it('should render without throwing an error', function() {
+    const wrapper = shallow(<Header/>)
+    expect(wrapper.contains()).toBe(true);
+  });
+
+//   it('should be selectable by class "foo"', function() {
+//     expect(shallow(<Foo />).is('.foo')).toBe(true);
+//   });
+
+//   it('should mount in a full DOM', function() {
+//     expect(mount(<Foo />).find('.foo').length).toBe(1);
+//   });
+
+//   it('should render to static HTML', function() {
+//     expect(render(<Foo />).text()).toEqual('Bar');
+//   });
+});
